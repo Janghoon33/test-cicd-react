07956ba7370ce81318d973d14fdb2460b7c16b2d
Janghoon33 <h970303@naver.com>
2022-10-14 12:06:32 +0900

    Edit app.js Helloworld!!!!!!

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          Hello World!!!!!!!!
+          Hello World!!!
       </header>
     </div>
   );
